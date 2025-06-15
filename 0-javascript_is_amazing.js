9d4a7e2f43645953e55d8d7109bfd0b815c4deb7
Marvin <marobdao@gmail.com>
2025-06-15 18:54:16 +0300

    0-javascript_is_amazing.js

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,3 @@
+let myVar = "JavaScript is amazing!";
+console.log(myVar); // Output: JavaScript is amazing!
+// This code declares a variable `myVar` and assigns it the string "JavaScript is amazing!".
